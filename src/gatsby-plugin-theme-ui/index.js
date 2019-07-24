298b36f539c3076a962df5448d9d84998ded7698
Eric Howey <ehowey@gmail.com>
2019-07-24 13:30:57 -0600

    Use deepmerge

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,6 +1,17 @@
-import merge from "lodash.merge"
-import { onePageTheme } from "gatsby-theme-catalyst-onepage"
+import merge from "deepmerge"
+import { baseTheme } from "gatsby-theme-catalyst-core"
 
-export default merge({}, onePageTheme, {
+export default merge(baseTheme, {
     //Updated theme options go here.
+    sizes: {
+        headerHeight: "60px", // Sets the height of the header on screens before the first breakpoint, 767px and below by default
+        headerHeightTablet: "80px", // Sets the height of the header after the first breakpoint, 768px to 1023px by default
+        headerHeightLaptop: "80px", // Sets the height of the header after the second breakpoint, 1024px and up by default
+        mainMarginTop: "60px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+        mainMarginTopTablet: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+        mainMarginTopLaptop: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+    },
+    catalystOptions: {
+        headerPosition: "fixed", //Fix the header for single page navigation ease
+    },
 })
